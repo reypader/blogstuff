4f447a2f05b8072d06380fdc3908f66b695a51bf
Reynald Pader <reynaldmpader@gmail.com>
2016-01-18 22:48:05 +0800

    Add JSON auth implementation to user Redis as a session storage

---
+++ spring-security-rest/json-auth-header-redis-session/src/main/java/com/rmpader/basicsecurity/security/Admin.java
@@ -0,0 +1,15 @@
+package com.rmpader.basicsecurity.security;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasRole('ADMIN')")
+public @interface Admin {
+
+}
